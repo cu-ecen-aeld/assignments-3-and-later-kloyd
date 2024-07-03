1c58c93edf362578225b26ee8224cfb6635b53d7
kloyd <kelly.loyd@icloud.com>
2024-07-03 16:05:26 -0500

    Start Thread, Lock/Unlock Mutex.

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -11,9 +11,32 @@
 void* threadfunc(void* thread_param)
 {
 
-    // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
+   
+
     // hint: use a cast like the one below to obtain thread arguments from your parameter
     //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
+    // Wait before obtaining mutex lock.
+    unsigned int usecs;
+    int result;
+    usecs = thread_func_args->wait_to_obtain_ms * 1000;
+    printf("***threadfunc sleeping for %d ms before obtaining mutex lock.\n", thread_func_args->wait_to_obtain_ms);
+    result = usleep(usecs);
+    if (result == 0) printf("sleep all okay.\n");
+    // obtain mutex lock.
+    printf("*** threadfunc get mutex lock.\n");
+    result = pthread_mutex_lock(thread_func_args->mutex);
+    if (result != 0) {
+        thread_func_args->thread_complete_success = false;
+    }
+    usecs = thread_func_args->wait_to_release_ms * 1000;
+    printf("*** threadfunc sleeping for %d ms before releasing mutex lock.\n", thread_func_args->wait_to_release_ms);
+    result = usleep(usecs);
+    if (result == 0) printf("sleep2 all okay.\n");
+    pthread_mutex_unlock(thread_func_args->mutex);
+    thread_func_args->thread_complete_success = true;
+    
     return thread_param;
 }
 
@@ -28,6 +51,20 @@ bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int 
      *
      * See implementation details in threading.h file comment block
      */
-    return false;
+    
+    struct thread_data* threadData = malloc(sizeof(struct thread_data));
+    threadData->wait_to_obtain_ms = wait_to_obtain_ms;
+    threadData->wait_to_release_ms = wait_to_release_ms;
+    threadData->mutex = mutex;
+    /* int pthread_create(pthread_t *thread, const pthread_attr_t *attr,
+                          void *(*start_routine) (void *), void *arg); */
+    int didcreate = pthread_create(thread, NULL, threadfunc, threadData);
+    //pthread_join(*thread, NULL);
+    if (didcreate == 0) {
+        return true;
+    } else {
+        return false;
+    }
+    //return threadData->thread_complete_success;
 }
 
